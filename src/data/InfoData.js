8dd74b8676c62e366ca6733bfef742218f307ef0
ANIASE <mpesejoao1992@gmail.com>
2022-05-10 14:30:22 +0100

    my project

---
+++ src/data/InfoData.js
@@ -0,0 +1,21 @@
+import ImageOne from '../images/view-1.jpg'
+import ImageTwo from '../images/interior-5.jpg'
+
+export const InfoData = {
+heading: 'Explore our luxury homes',
+paragraphOne: 'We have amazing homes for our custumer, with great view.',
+paragraphTwo: 'We have amazing homes for our custumer, with great view.',
+buttonLabel: 'View Homes',
+image: ImageOne,
+reverse: false,
+delay: 100
+}
+export const InfoDataTwo = {
+heading: 'Modern Houses',
+paragraphOne: 'We have amazing homes for our custumer, with great view.',
+paragraphTwo: 'We have amazing homes for our custumer, with great view.',
+buttonLabel: 'View Homes',
+image: ImageTwo,
+reverse: true,
+delay: 300
+}
---
+++ src/data/SliderData.js
@@ -0,0 +1,40 @@
+import ImageOne from '../images/house-1.jpg'
+import ImageTwo from '../images/house-2.jpg'
+import ImageThree from '../images/house-3.jpg'
+import ImageFour from '../images/house-4.jpg'
+
+
+export const SliderData = [
+    {
+        title: 'Luxury Villa in Kigali, Rwanda',
+        price: '$4,200,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: 'House in San Jose, Costa Rica',
+        price: '$2,200,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'House in Luanda , Angola',
+        price: '$3,200,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House'
+    },
+    {
+        title: 'House in Cape Town, South Africa',
+        price: '$5,200,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House'
+    },
+]
